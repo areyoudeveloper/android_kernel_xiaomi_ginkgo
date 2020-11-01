d181e02859b1acaa0550c95b86b4622050376cb9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-11-01 18:34:33 +0530

    cpu_input_boost: Disable boosting when battery saver is on
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -5,6 +5,7 @@
 
 #define pr_fmt(fmt) "cpu_input_boost: " fmt
 
+#include <linux/battery_saver.h>
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
 #include <linux/msm_drm_notify.h>
@@ -120,6 +121,9 @@ bool cpu_input_boost_within_input(unsigned long timeout_ms)
 static void __cpu_input_boost_kick(struct boost_drv *b)
 {
 	if (test_bit(SCREEN_OFF, &b->state) || is_battery_saver_on())
+		return;
+
+	if (!input_boost_duration)
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
