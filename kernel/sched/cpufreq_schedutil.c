b128a4b99543e779005ab3b33905b9052243c16f
areyoudeveloper <kmd36rk@gmail.com>
2023-10-08 19:39:29 +0700

    kernel: sched/cpufreq_schedutil: fix multipple definetion

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -543,9 +543,6 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	return count;
 }
 
-static struct governor_attr up_rate_limit_us = __ATTR_RW(up_rate_limit_us);
-static struct governor_attr down_rate_limit_us = __ATTR_RW(down_rate_limit_us);
-
 static ssize_t iowait_boost_enable_show(struct gov_attr_set *attr_set,
 					char *buf)
 {
