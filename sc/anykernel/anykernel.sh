440faa856afa5a0b3b1977ffee9178a6646210dc
TeaLovers(TEA) <kmd36rk@gmail.com>
2023-10-10 00:42:14 +0700

    anykernel.sh: fix a template ci build

--- sc/anykernel/anykernel.sh
+++ sc/anykernel/anykernel.sh
@@ -1,47 +1,61 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string="Quarzt Crystal 4.14"
-do.devicecheck=0
+kernel.string=Quartz Kernel for Redmi Note 8/8T
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=ginkgo
-supported.versions=
-'; }
+device.name2=willow
+device.name3=
+device.name4=
+device.name5=
+supported.versions=11.0-13.0
+supported.patchlevels=
+'; } # end properties
 
+# shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-patch_vbmeta_flag=0;
+patch_vbmeta_flag=auto;
 
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
-# If lk2nd is installed, Read boot from 1MB offset
-if [ "$(dd if=${block} skip=64 bs=1 count=5 2>/dev/null)" == "lk2nd" ]; then
-  ui_print "Detected lk2nd installation! Skipping the first 1MB."
-  customdd="bs=1M skip=1"
-  lk2nd=1
-fi
-dump_boot;
-# Enable USB ConfigFS
-patch_cmdline androidboot.usbconfigfs androidboot.usbconfigfs=true
-ui_print "*******************************************"
-ui_print "Updating Kernel and Patching cmdline..."
-ui_print "*******************************************"
-# If lk2nd is installed, Write boot to 1MB offset
-if [ "$lk2nd" == "1" ]; then
-  customdd="bs=1M seek=1"
-  # GNU GREP: lk2nd_end=$(expr $(grep --byte-offset --only-matching --text SEANDROIDENFORCE ${block}|cut -d ':' -f 1) + 16)
-  # AK3 grep: lk2nd_end=$(expr $(grep -o -n SEANDROIDENFORCE ${block}|cut -d ':' -f 1) + 16)
-  # Recovery grep:
-  lk2nd_end=$(expr $(/bin/grep -o -b -a SEANDROIDENFORCE ${block}|cut -d ':' -f 1) + 16)
-  lk2nd_gaps=$(expr 1048576 - ${lk2nd_end})
-  ui_print "Filling ${lk2nd_gaps} bytes of gap from lk2nd data (${lk2nd_end}) till 1MB with zeroes."
-  dd if=/dev/zero of=$block bs=1 count=$lk2nd_gaps seek=$lk2nd_end
-fi
 
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+
+## AnyKernel boot install
+dump_boot;
 write_boot;
+## end boot install
+
+
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
+
