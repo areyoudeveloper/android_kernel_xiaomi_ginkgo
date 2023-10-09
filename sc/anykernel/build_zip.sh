e6643525d7cd3f1a2b23fb655ba4f2daa4385694
TeaLovers(TEA) <kmd36rk@gmail.com>
2023-10-10 01:43:00 +0700

    build_zip.sh: include dtbo

--- sc/anykernel/build_zip.sh
+++ sc/anykernel/build_zip.sh
@@ -2,6 +2,7 @@
 
 ak_dir=anykernel/any
 image=out/arch/arm64/boot/Image.gz-dtb
+dtbimg=out/arch/arm64/boot/dtbo.img
 pre=` cat out/.config | grep Linux | cut -f 3 -d " "  `
 fmt=`date +%d\.%m\.%y_%r | sed 's/:/./g;s/ PM/.PM/g;s/ AM/.AM/g;s/ IST//g'`
 
@@ -13,8 +14,9 @@ rm $ak_dir/anykernel.sh
 cp anykernel/anykernel.sh $ak_dir
 #cp -f anykernel/ak3-core.sh $ak_dir/tools/
 cp -rf $image $ak_dir/
+cp -rf $dtbimg $ak_dir/
 cd $ak_dir
 
-zip -r9 ${pre}_QuartzCrystal-4.14_${model}_${fmt}.zip *
+zip -r9 QuartzCrystal-4.14_${model}_${fmt}.zip *
 mv *.zip ../../
 cd ../../
