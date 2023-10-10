9bfab8ddd9778696c9d9393ca21b1ff342a328ce
TeaLovers(TEA) <kmd36rk@gmail.com>
2023-10-10 21:57:15 +0700

    build_zip.sh: dont be weird

--- sc/anykernel/build_zip.sh
+++ sc/anykernel/build_zip.sh
@@ -6,13 +6,6 @@ dtbimg=out/arch/arm64/boot/dtbo.img
 pre=` cat out/.config | grep Linux | cut -f 3 -d " "  `
 fmt=`date +%d\.%m\.%y_%r | sed 's/:/./g;s/ PM/.PM/g;s/ AM/.AM/g;s/ IST//g'`
 
-rm -rf anykernel3
-
-mkdir -p $ak_dir 
-git clone --depth=1 https://github.com/osm0sis/AnyKernel3 $ak_dir
-rm $ak_dir/anykernel.sh
-cp -rf anykernel/any/* $ak_dir
-#cp -f anykernel/ak3-core.sh $ak_dir/tools/
 cp -rf $image $ak_dir/
 cp -rf $dtbimg $ak_dir/
 cd $ak_dir
