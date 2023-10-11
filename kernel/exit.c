cd5940f508260e32655875f3fcc2d6a2af48bf31
areyoudeveloper <kmd36rk@gmail.com>
2023-10-11 19:40:43 +0700

    mm:fix incompatible pointer

--- kernel/exit.c
+++ kernel/exit.c
@@ -599,9 +599,8 @@ static void exit_mm(void)
 	if (test_thread_flag(TIF_MEMDIE))
 		exit_oom_victim();
 	if (mm_released)
-		set_tsk_thread_flag(current, TIF_MM_RELEASED);=
+		set_tsk_thread_flag(current, TIF_MM_RELEASED);
 
-	mmput(mm);
 #ifdef CONFIG_ANDROID_SIMPLE_LMK
 	clear_thread_flag(TIF_MEMDIE);
 #else
