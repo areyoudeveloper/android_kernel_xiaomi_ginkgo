824bd682d27a9a0c13e23c4e5d11b527558fc24a
areyoudeveloper <kmd36rk@gmail.com>
2023-10-12 20:08:47 +0700

    mm/mm_alloc.c: fix wrong defined

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4285,7 +4285,7 @@ retry:
 		goto nopage;
 
 	/* Boost when memory is low so allocation latency doesn't get too bad */
-	devfreq_boost_kick_max(DEVFREQ_CPU_DDR_BW, 75);
+	devfreq_boost_kick_max(DEVFREQ_CPU_CPU_DDR_BW, 75);
 
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
