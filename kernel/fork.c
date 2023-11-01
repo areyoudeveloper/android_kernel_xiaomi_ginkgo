89a94639d6c4027a668f128b8f66824b531cad4f
TeaLovers(TEA) <kmd36rk@gmail.com>
2023-11-01 17:49:54 +0700

    kernel:fork.c : only boost ddr

--- kernel/fork.c
+++ kernel/fork.c
@@ -2257,7 +2257,6 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
-		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_CPU_DDR_BW, 50);
 	}
 
