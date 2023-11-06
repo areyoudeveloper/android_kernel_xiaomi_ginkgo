ff47156e66ff1b622d5b773773c19a67d368dca4
TeaLovers(TEA) <kmd36rk@gmail.com>
2023-11-06 15:35:32 +0700

    build_helper.sh: use google-clang17.0.4 from my repo

--- sc/anykernel/build_helper.sh
+++ sc/anykernel/build_helper.sh
@@ -2,7 +2,7 @@
 
 [ -d out ] && rm -rf out && mkdir -p out || mkdir -p out
 
-TC_DIR=${HOME}/android/TOOLS/r416183b
+TC_DIR=${HOME}/android/TOOLS/google-clang17.0.4
 GCC_DIR=${HOME}/android/TOOLS/aarch64-linux-gnu
 GCC32_DIR=${HOME}/android/TOOLS/gcc32
 CFG_DIR=$(pwd)/arch/arm64/configs/
@@ -12,19 +12,19 @@ export KBUILD_KVER=4.14.328-QuartzCrystal💎
 && echo -e "\nG-Clang Present.\n" \
 || echo -e "\nG-Clang Not Present. Downloading Around 800MB...\n" \
 | mkdir -p $TC_DIR && mkdir -p $GCC_DIR && mkdir -p $GCC32_DIR \
-| git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b main --depth 1 $TC_DIR \
+| git clone https://gitlab.com/areyoudeveloper/google-clang17.0.4 -b 17 --depth 1 $TC_DIR \
 && git clone https://github.com/najahiiii/aarch64-linux-gnu -b gcc8-201903-A --depth 1 $GCC_DIR \
 && git clone https://github.com/najahiiii/aarch64-linux-gnu -b 4.9-32-mirror --depth 1 $GCC32_DIR \
 | echo "Done."
 
 echo -e "\nChecking Clang Version...\n"
-PATH="$TC_DIR/clang-r498229b/bin:${PATH}" clang --version
+PATH="$TC_DIR/bin:${PATH}" clang --version
 echo -e "\n\nStarting Build...\n"
 
 cp $CFG $CFG_DIR/final_defconfig
 
 pcmake() {
-PATH="$TC_DIR/clang-r498229b/bin:$GCC_DIR/bin:$GCC32_DIR/bin:${PATH}" \
+PATH="$TC_DIR/bin:$GCC_DIR/bin:$GCC32_DIR/bin:${PATH}" \
 make	\
 	O=out \
 	ARCH=arm64 \
