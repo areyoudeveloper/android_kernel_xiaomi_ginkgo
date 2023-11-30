bf930d382cc1c94584998adfbc839d8828da765c
areyoudeveloper <kmd36rk@gmail.com>
2023-11-30 10:45:31 +0700

    Revert a6b0aa18b46b11e6695e100d1fc8b26f83d1f9c4

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -246,13 +246,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 	if (work_pending(&input_boost_work))
 		return;
 
-//	queue_work(cpu_boost_wq, &input_boost_work);
-
-	if ((type == EV_KEY && code == KEY_POWER) ||
-		(type == EV_KEY && code == KEY_WAKEUP)) {
-		kthread_queue_work(&cpu_boost_worker, &powerkey_input_boost_work);
-	} else
-		kthread_queue_work(&cpu_boost_worker, &input_boost_work);
+	queue_work(cpu_boost_wq, &input_boost_work);
 
 	last_input_time = ktime_to_us(ktime_get());
 }
